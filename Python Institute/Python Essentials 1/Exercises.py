7f43cd48e76ace94e9f9924d3a87e4ae63088c9f
engrvitalis <ndlinks@gmail.com>
2025-01-18 22:54:51 +0100

    Exercise 2.6.1.10 added

--- Python Institute/Python Essentials 1/Exercises.py
+++ Python Institute/Python Essentials 1/Exercises.py
@@ -75,3 +75,17 @@ kilometers_to_miles = kilometers / 1.61
 print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
 print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
 
+# Exercise 2.4.1.10
+x =  -1
+x = float(x)
+# write your code here
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
+# Exercise 2.6.1.10
+x = float(input("Enter value for x: "))
+
+# Write your code here.
+y = 1/(x + (1/(x + (1/(x + (1/x))))))
+
+print("y =", y)
