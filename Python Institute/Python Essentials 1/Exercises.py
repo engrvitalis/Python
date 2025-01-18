8e2c5ec42ed719c3d5f18a1cb2a714f7bb35be3c
engrvitalis <ndlinks@gmail.com>
2025-01-18 07:11:49 +0100

    "I'm learning Python" exercise added.

--- Python Institute/Python Essentials 1/Exercises.py
+++ Python Institute/Python Essentials 1/Exercises.py
@@ -43,3 +43,14 @@ print("    *       *      "*2)
 print("    *       *      "*2)
 print("    *********      "*2)
 
+# Write a one-line piece of code, using the print() function, 
+# as well as the newline and escape characters, 
+# to match the expected result outputted on three lines.
+
+# Expected output
+
+# "I'm"
+# ""learning""
+# """Python"""
+
+print("\"I\'m\"", "\"\"learning\"\"", "\"\"\"Python\"\"\"", sep="\n")
