8a8dd6efe49505aa1809f59dce0c86987cbdcaea
engrvitalis <ndlinks@gmail.com>
2025-01-21 22:42:27 +0100

    collatz's hypothesis and pyramid exercises added

--- Python Institute/Python Essentials 1/pyramid.py
+++ Python Institute/Python Essentials 1/pyramid.py
@@ -1,9 +1,11 @@
 blocks = int(input("Enter the number of blocks: "))
+i = 0
 
-for i in range(1, blocks+1):
+while True:
     blocks -= i
     if blocks >= 0:
         height = i
     else:
         break
+    i += 1
 print("The height of the pyramid:", height)
